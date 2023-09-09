63312aab016b79ad88e388c621105eecf32dc444
Hongwei Xi <gmhwxi@gmail.com>
2023-09-09 15:58:12 -0400

    Updating: very very minorly

--- assigns/assign0/assign0.ml
+++ assigns/assign0/assign0.ml
@@ -54,6 +54,7 @@ Please implement a function that converts a given
 string to an integer:
 fun str2int(cs: string): int
 In particular, it is expected that str2int(int2str(x)) = x
+for natural numbers x.
 (You can assume that the given string is a sequence of digits)
 (And the empty sequence represents the integer 0)
 *)
