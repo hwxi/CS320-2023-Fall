86eea459a8e6ab1ca1e319bdfc28d41a3ecbb129
Hongwei Xi <gmhwxi@gmail.com>
2023-09-12 16:11:45 -0400

    Updating: very very minorly

--- assigns/assign0/README.md
+++ assigns/assign0/README.md
@@ -5,10 +5,10 @@
 - Out: Wed, the 6th of September at 12:00am
 - Due: Tue, the 12th of September at 11:59pm
 
-### assign0.sml
+### assign0.ml
 
 The description of Assign0 is given in the
-file assign0.sml. If you are unclear about
+file assign0.ml. If you are unclear about
 what is asked of you to do in this assignment,
 please make sure that you seek clarification
 before proceeding.
@@ -25,8 +25,8 @@ This is a directory for you. Please store in this directory all the
 code that you want to turn in. Your submitted files should be named
 as follows:
 
-1. MySolution/assign0-1.sml // containing your solution to assign0-1
-2. MySolution/assign0-2.sml // containing your solution to assign0-2
-3. MySolution/assign0-3.sml // containing your solution to assign0-3
-4. MySolution/assign0-4.sml // containing your solution to assign0-4
-5. MySolution/assign0-5.sml // containing your solution to assign0-5
+1. MySolution/assign0-1.ml // containing your solution to assign0-1
+2. MySolution/assign0-2.ml // containing your solution to assign0-2
+3. MySolution/assign0-3.ml // containing your solution to assign0-3
+4. MySolution/assign0-4.ml // containing your solution to assign0-4
+5. MySolution/assign0-5.ml // containing your solution to assign0-5
--- classlib/OCaml/MyOCaml.ml
+++ classlib/OCaml/MyOCaml.ml
@@ -66,11 +66,54 @@ let
 
 (* ****** ****** *)
 
+type
+('xs, 'x0) forall =
+'xs -> ('x0 -> bool) -> bool
+
+type
+('xs, 'x0) foreach =
+'xs -> ('x0 -> unit) -> unit
+type
+('xs, 'x0) rforeach =
+'xs -> ('x0 -> unit) -> unit
+
+type
+('xs, 'x0) listize = 'xs -> 'x0 list
+type
+('xs, 'x0) arrnize = 'xs -> 'x0 array
+
+type
+('xs, 'x0) rlistize = 'xs -> 'x0 list
+type
+('xs, 'x0) rarrnize = 'xs -> 'x0 array
+
+type
+('xs, 'x0, 'y0) map_list =
+'xs -> ('x0 -> 'y0) -> 'y0 list
+type
+('xs, 'x0, 'y0) maprev_list =
+'xs -> ('x0 -> 'y0) -> 'y0 list
+
+type
+('xs, 'x0, 'r0) foldleft =
+'xs -> 'r0 -> ('r0 -> 'x0 -> 'r0) -> 'r0
+type
+('xs, 'x0, 'r0) foldright =
+'xs -> 'r0 -> ('x0 -> 'r0 -> 'r0) -> 'r0
+
+(* ****** ****** *)
+
 let
 int1_foreach
 (n0:int)
 (work: int -> unit): unit =
 for i0 = 0 to n0-1 do work(i0) done
+
+let
+int1_rforeach
+(n0:int)
+(work: int -> unit): unit =
+for i0 = 0 to n0-1 do work(n0-1-i0) done
 
 (* ****** ****** *)
 
@@ -112,8 +155,7 @@ string_tolower
 (cs: string): string =
 string_init
 (string_length(cs))
-(
- fun i0 ->
+(fun i0 ->
  char_tolower(string_get_at(cs)(i0)))
 
 (* ****** ****** *)
@@ -126,13 +168,39 @@ int1_foreach
 (string_length(cs))
 (fun i0 -> work(string_get_at(cs)(i0)))
 
+let
+string_rforeach
+(cs: string)
+(work: char -> unit) =
+int1_rforeach
+(string_length(cs))
+(fun i0 -> work(string_get_at(cs)(i0)))
+
 (* ****** ****** *)
 
 let
-string_tabulate
-(n0: int)
-(f0: int -> char): string = String.init n0 f0
+string_tabulate = String.init
 ;;
+(* ****** ****** *)
+
+let
+list_arrnize
+(xs: 'a list): 'a array = Array.of_list(xs)
+
+(* ****** ****** *)
+
+let rec
+list_revapp
+(xs: 'a list)
+(ys: 'a list): 'a list =
+match xs with
+| [] -> ys
+| x1 :: xs -> list_revapp(xs)(x1 :: ys)
+
+let
+list_reverse
+(xs: 'a list): 'a list = list_revapp(xs)([])
+
 (* ****** ****** *)
 
 let rec
@@ -165,13 +233,13 @@ match xs with
 | x1 :: xs -> (work(x1); list_foreach(xs)(work)))
 ;;
 (* ****** ****** *)
+(* ****** ****** *)
+(* ****** ****** *)
 
 let
 forall_to_foreach
 ( forall
-: 'xs -> ('x0 -> bool) -> bool
-)
-: 'xs -> ('x0 -> unit) -> unit =
+: ('xs, 'x0) forall): ('xs, 'x0) foreach =
 fun(xs)(work) ->
 let _ =
   forall(xs)(fun(x0) -> (work(x0); true)) in ()
@@ -181,9 +249,7 @@ let _ =
 let
 foreach_to_forall
 ( foreach
-: 'xs -> ('x0 -> unit) -> unit
-)
-: 'xs -> ('x0 -> bool) -> bool =
+: ('xs, 'x0) foreach): ('xs, 'x0) forall =
 fun(xs)(test) ->
 try
 let
@@ -201,8 +267,7 @@ with False(*void*) -> (false)
 let
 foreach_to_foldleft
 ( foreach
-: 'xs -> ('x0 -> unit) -> unit
-)
+: ('xs, 'x0) foreach)
 : 'xs -> 'r0 -> ('r0 -> 'x0 -> 'r0) -> 'r0 =
 fun
 (xs)(r0)(fopr) ->
@@ -215,42 +280,206 @@ foreach(xs)
 (* ****** ****** *)
 
 let rec
+foreach_to_map_list
+( foreach
+: ('xs, 'x0) foreach)
+: ('xs, 'x0, 'y0) map_list =
+fun(xs)(fopr) ->
+list_reverse
+(foreach_to_maprev_list(foreach)(xs)(fopr))
+
+and
+foreach_to_maprev_list
+( foreach
+: ('xs, 'x0) foreach)
+: ('xs, 'x0, 'y0) maprev_list =
+fun
+(xs)(fopr) ->
+let
+res = ref([]) in
+foreach(xs)
+(fun(x0) -> res := fopr(x0) :: !res); !res
+;;(* end of [foreach_to_maprev_list]: let *)
+
+(* ****** ****** *)
+
+let rec
+foreach_to_listize
+( foreach
+: ('xs, 'x0) foreach
+) : ('xs, 'x0) listize =
+fun(xs) ->
+foreach_to_map_list(foreach)(xs)(fun x -> x)
+
+let rec
+foreach_to_rlistize
+( foreach
+: ('xs, 'x0) foreach
+) : ('xs, 'x0) rlistize =
+fun(xs) ->
+foreach_to_maprev_list(foreach)(xs)(fun x -> x)
+
+(* ****** ****** *)
+
+let rec
+foreach_to_arrnize
+( foreach
+: ('xs, 'x0) foreach
+) : ('xs, 'x0) arrnize =
+(
+ fun xs ->
+ list_arrnize(foreach_to_listize(foreach)(xs)))
+
+let rec
+foreach_to_rarrnize
+( foreach
+: ('xs, 'x0) foreach
+) : ('xs, 'x0) rarrnize =
+(
+ fun xs ->
+ list_arrnize(foreach_to_rlistize(foreach)(xs)))
+
+(* ****** ****** *)
+
+let rec
 foreach_to_length
 ( foreach
-: 'xs -> ('x0 -> unit) -> unit)
-: ('xs -> int) =
+: ('xs, 'x0) foreach): 'xs -> int =
 foldleft_to_length(foreach_to_foldleft(foreach))
 
 and
 foldleft_to_length
 ( foldleft
-: 'xs -> int -> (int -> 'x0 -> int) -> int)
-: ('xs -> int) =
+: ('xs,'x0,'r0) foldleft): 'xs -> int =
 (
- fun(xs) -> foldleft(xs)(0)(fun(r0)(x0) -> r0+1)
-)
+fun(xs) -> foldleft(xs)(0)(fun(r0)(x0) -> r0+1))
+;;
+(* ****** ****** *)
+
+let
+rforeach_to_foldright
+( rforeach
+: ('xs, 'x0) rforeach)
+: ('xs, 'x0, 'r0) foldright =
+fun
+(xs)(r0)(fopr) ->
+let
+res = ref(r0) in
+rforeach(xs)
+(fun(x0) -> res := fopr(x0)(!res)); !res
+;;(* end of [rforeach_to_foldright]: let *)
+
+(* ****** ****** *)
+(* ****** ****** *)
+(* ****** ****** *)
+
+let
+int1_forall(n0) =
+foreach_to_forall(int1_foreach)(n0)
+;;
+let
+string_forall(cs) =
+foreach_to_forall(string_foreach)(cs)
+;;
+
+(* ****** ****** *)
+(* ****** ****** *)
+(* ****** ****** *)
+
+let
+int1_listize(n0) =
+foreach_to_listize(int1_foreach)(n0)
+let
+int1_rlistize(n0) =
+foreach_to_rlistize(int1_foreach)(n0)
+
+let
+string_listize(cs) =
+foreach_to_listize(string_foreach)(cs)
+let
+string_rlistize(cs) =
+foreach_to_rlistize(string_foreach)(cs)
+
+(* ****** ****** *)
+
+let
+int1_foldleft(n0) =
+foreach_to_foldleft(int1_foreach)(n0)
+let
+string_foldleft(cs) =
+foreach_to_foldleft(string_foreach)(cs)
+;;
+let
+int1_foldright(n0) =
+rforeach_to_foldright(int1_rforeach)(n0)
+let
+string_foldright(cs) =
+rforeach_to_foldright(string_rforeach)(cs)
+;;
+
+(* ****** ****** *)
+(* ****** ****** *)
+(* ****** ****** *)
+
+let
+list_make_foreach
+( xs: 'xs)
+( foreach
+: ('xs, 'x0) foreach): 'x0 list =
+(
+  foreach_to_listize(foreach)(xs))
+let
+list_make_rforeach
+( xs: 'xs)
+( rforeach
+: ('xs, 'x0) rforeach): 'x0 list =
+(
+  foreach_to_rlistize(rforeach)(xs))
+;;
+(* ****** ****** *)
+
+let
+string_make_foreach
+( xs: 'xs)
+( foreach
+: ('xs, char) foreach): string =
+let
+xs =
+foreach_to_arrnize(foreach)(xs) in
+String.init (Array.length(xs)) (fun i -> xs.(i))
+;;
+let
+string_make_rforeach
+( xs: 'xs)
+( rforeach
+: ('xs, char) rforeach): string =
+let
+xs =
+foreach_to_rarrnize(rforeach)(xs) in
+String.init (Array.length(xs)) (fun i -> xs.(i))
 ;;
 (* ****** ****** *)
 (* ****** ****** *)
 (* ****** ****** *)
 
 let
-int1_forall = foreach_to_forall(int1_foreach)
-;;
-let
-string_forall = foreach_to_forall(string_foreach)
-;;
+list_append
+(xs: 'a list)
+(ys: 'a list): 'a list =
+list_make_foreach
+(xs,ys)
+(
+fun(xs, ys) work ->
+(list_foreach xs work; list_foreach ys work))
 
-(* ****** ****** *)
-(* ****** ****** *)
-(* ****** ****** *)
-
 let
-int1_foldleft = foreach_to_foldleft(int1_foreach)
-;;
-let
-string_foldleft = foreach_to_foldleft(string_foreach)
-;;
+list_concat
+(xss: 'a list list): 'a list =
+list_make_foreach
+( xss )
+(
+fun xss work ->
+list_foreach xss (fun xs -> list_foreach xs work))
 
 (* ****** ****** *)
 (* ****** ****** *)
