5f0fc8f8c229e5e836fc458bdecf5a34a522f1ba
Hongwei Xi <gmhwxi@gmail.com>
2023-09-23 02:18:16 -0400

    Updating: very very minorly

--- assigns/assign2/Testing/OCaml/assign2-3-test.ml
+++ assigns/assign2/Testing/OCaml/assign2-3-test.ml
@@ -14,9 +14,11 @@ fun work ->
 list_iforeach([1;2;3;4;5])(fun i x -> work(i, x)))
 ;;
 (* ****** ****** *)
-let _ =
+let () =
+assert
+(
 list_forall
-(ixs)(fun ix -> let (i, x) = ix in i+1 = x);;
+(ixs)(fun ix -> let (i, x) = ix in i+1 = x));;
 (* ****** ****** *)
 let () = print_string("Assign2-3-test passed!\n");;
 (* ****** ****** *)
