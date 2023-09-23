f2c1e7e52c08ba0608dd3323c79ae19a8f9c35d4
Hongwei Xi <gmhwxi@gmail.com>
2023-09-23 19:42:00 -0400

    Updating: very very minorly

--- lectures/lecture-09-26/exception.ml
+++ lectures/lecture-09-26/exception.ml
@@ -4,6 +4,38 @@ exception True
 exception False
 (* ****** ****** *)
 
+let rec
+list_mul0(xs: int list): int =
+match xs with
+| [] -> 1
+| x1 :: xs -> x1 * list_mul0(xs)
+
+let rec
+list_mul1(xs: int list): int =
+match xs with
+| [] -> 1
+| x1 :: xs ->
+  if x1 = 0 then 0 else list_mul1(xs)
+
+exception Zero
+
+let rec
+list_mul2(xs: int list): int =
+try
+match xs with
+| [] -> 1
+| x1 :: xs ->
+  if x1 = 0 then raise Zero else list_mul2(xs)
+with Zero -> 0
+
+(* ****** ****** *)
+
+let test0 = list_mul0[1;2;3;0;4;5]
+let test1 = list_mul1[1;2;3;0;4;5]
+let test2 = list_mul2[1;2;3;0;4;5]
+
+(* ****** ****** *)
+
 let
 foreach_to_forall
 ( foreach
---
+++ lectures/lecture-09-26/option.ml
@@ -0,0 +1,27 @@
+(* ****** ****** *)
+#use "./../../classlib/OCaml/MyOCaml.ml";;
+(* ****** ****** *)
+
+(*
+ An option-value is
+ just a list of length at most 1
+*)
+
+type 'a option = None | Some of 'a
+
+(* ****** ****** *)
+
+let
+foreach_to_mapopt_list(foreach) =
+fun xs fopr ->
+list_make_fwork
+(
+fun work ->
+foreach(xs)(fun x ->
+let opt = fopr(x) in
+match opt with None -> () | Some y -> work(y)))
+
+;;
+(* ****** ****** *)
+
+(* end of [lecture-09-26-option.ml] *)  
