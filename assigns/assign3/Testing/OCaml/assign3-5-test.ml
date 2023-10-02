bc9cd3b6b099f9b0c0f9106b6a4de756d31e0cf4
Hongwei Xi <gmhwxi@gmail.com>
2023-10-01 20:40:28 -0400

    Updating: very very minorly

--- assigns/assign3/Testing/OCaml/assign3-5-test.ml
+++ assigns/assign3/Testing/OCaml/assign3-5-test.ml
@@ -10,6 +10,20 @@ assert(List.length(the_solutions) = 92);;
 (* ****** ****** *)
 
 let
+board_foreach
+(bd: board_t)
+(work: int -> unit) =
+let
+(x0, x1, x2, x3, x4, x5, x6, x7) = bd
+in
+work(x0); work(x1);
+work(x2); work(x3);
+work(x4); work(x5); work(x6); work(x7)
+;; (*end-of-[board_foreach]*)
+
+(* ****** ****** *)
+
+let
 board_sum(bd: board_t): int =
 foreach_to_foldleft
 (board_foreach)(bd)(0)(fun res p -> res+p)
