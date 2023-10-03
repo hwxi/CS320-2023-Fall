b0e747cfd698c9c13f652753f53362b0115bbb45
Hongwei Xi <gmhwxi@gmail.com>
2023-10-03 10:30:49 -0400

    Updating: very very minorly

---
+++ lectures/lecture-10-03/gtree-bfs.ml
@@ -0,0 +1,34 @@
+(* ****** ****** *)
+
+type 'a strcon =
+  StrNil
+| StrCons of
+  'a * (unit -> 'a strcon)
+
+(* ****** ****** *)
+
+type 'a stream =
+unit -> 'a strcon (* thunk *)
+
+(* ****** ****** *)
+
+type node = NODE of int
+
+(* ****** ****** *)
+
+let rec
+gtree_bfs
+( nxs
+: node list)
+( fchildren
+: node -> node list): node stream = fun() ->
+(
+match nxs with
+  [] -> StrNil
+| nx1 :: nxs ->
+  StrCons(nx1, gtree_bfs(nxs @ fchildren(nx1))(fchildren))
+)
+;;
+(* ****** ****** *)
+
+(* end of [CS320-2023-Spring-lectures-gtree-bfs.ml] *)
---
+++ lectures/lecture-10-03/gtree-dfs.ml
@@ -0,0 +1,34 @@
+(* ****** ****** *)
+
+type 'a strcon =
+  StrNil
+| StrCons of
+  'a * (unit -> 'a strcon)
+
+(* ****** ****** *)
+
+type 'a stream =
+unit -> 'a strcon (* thunk *)
+
+(* ****** ****** *)
+
+type node = NODE of int
+
+(* ****** ****** *)
+
+let rec
+gtree_dfs
+( nxs
+: node list)
+( fchildren
+: node -> node list): node stream = fun() ->
+(
+match nxs with
+  [] -> StrNil
+| nx1 :: nxs ->
+  StrCons(nx1, gtree_dfs(fchildren(nx1) @ nxs)(fchildren))
+)
+;;
+(* ****** ****** *)
+
+(* end of [CS320-2023-Spring-lectures-gtree-dfs.sml] *)
