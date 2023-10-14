e88ee49e9dbd8fb057eb826007c44ea37a088522
Hongwei Xi <gmhwxi@gmail.com>
2023-10-14 11:35:51 -0400

    Updating: very very minorly

---
+++ exams/midterm/review.ml
@@ -0,0 +1,49 @@
+(*
+## Midterm Review
+*)
+
+(*
+## Names vs. Variables
+*)
+
+## Recursive functions
+
+Functions are first-class values
+
+## Tail-Recursion
+Loops are tail-recursive functions
+(However, many popular mainstream
+programming languages (e.g., Python)
+does not support tail-recursive call
+optimization, which is rather unfortuate!)
+
+## Higher-order functions
+Loops are 2nd-order functions
+Loop transformers are 3rd-order functions
+
+## Mutual Recursion
+
+For instance, ...
+
+## Datatypes and Pattern Matching
+
+There features are in the core of ML-like
+functional programming languages; they, for
+instance, can be used to greatly facilitate
+compiler construction.
+
+Here are some common datatypes
+
+type 'a option =
+  None | Some of 'a
+
+type 'a list =
+  Nil | Cons of 'a * 'a list
+
+## Option-based error handling
+
+## Exception-based error handling
+
+## Continuation
+
+## Generators (LINEAR streams)
