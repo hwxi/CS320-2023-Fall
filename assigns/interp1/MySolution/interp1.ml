4ac58d0a1f4b105b7b771b392dffebe32998d41f
qcfu <qcfu@bu.edu>
2023-11-13 23:11:11 -0500

    interp project 1

---
+++ assigns/interp1/MySolution/interp1.ml
@@ -0,0 +1,15 @@
+#use "./../../../classlib/OCaml/MyOCaml.ml";;
+
+(*
+
+Please implement the interp function following the
+specifications described in CS320_Fall_2023_Project-1.pdf
+
+Notes:
+1. You are only allowed to use library functions defined in MyOCaml.ml
+   or ones you implement yourself.
+2. You may NOT use OCaml standard library functions directly.
+
+*)
+
+let interp (s : string) : string list option = (* YOUR CODE *)
